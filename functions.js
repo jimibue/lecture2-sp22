270a15af4eb018b2d1ff00e7d9af022cad4de1a6
James <jimibue@gmail.com>
2022-02-15 11:20:22 -0800

    function changed

--- functions.js
+++ functions.js
@@ -22,11 +22,36 @@ sayHola()
 // think of a param as variable that is only visible to the function
 function introducePerson(person){
     console.log(`Hello ${person}, I'm James`)
-
 }
 
 // here pass function an argument
 introducePerson('Jon')
 introducePerson('Jill')
+
+function add(num1, num2){
+   return num1 + num2
+}
+
+// console.log(typeof 2) //number
+// console.log(2 === 3) //false
+// console.log(2 === 2) //true
+// console.log(add('1','1')) //'11'
+// console.log(add(1,'1')) //'11'
+let sum = add(1,1)
+let sum1 = add(add(2,2),add(3,3))
+console.log(sum1)
+
+
+// once a function returns it is quits
+const evenOrOdd = (num)=>{
+    if(num % 2 === 0){
+        return 'even'
+    }else{
+        return 'odd'
+    }
+}
+
+let x = evenOrOdd(3)
+console.log(x) //'odd'
 
 
